c603bfdf58dc59442b58703c2a98df243a551727
Selwyn Nobleza <selwynnobleza02@gmail.com>
2026-07-11 11:27:13 +0800

    React plus Vite Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LandingPage from "./landing_page/LandingPage";
+
+function App() {
+  return (
+    <div className="app">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
