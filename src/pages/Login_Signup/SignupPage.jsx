5c8f1f89332cc4a4bf9417d405153d187aaeed8c
namron16 <namronpontillo@gmail.com>
2026-07-14 19:22:21 +0800

    fix: authcontext import on signup page

--- src/pages/Login_Signup/SignupPage.jsx
+++ src/pages/Login_Signup/SignupPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import logo from "../../assets/logo.png";
 import styles from "./SignupPage.module.css";
 
